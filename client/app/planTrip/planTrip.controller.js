437e62d96ea5f2d775583dac39a7d267fdae0fbd
Justin <justinbchristensen@gmail.com>
2014-09-16 20:56:32 -0400

    Got Sidebar in and data pulling in in raw form

--- client/app/planTrip/planTrip.controller.js
+++ client/app/planTrip/planTrip.controller.js
@@ -15,12 +15,14 @@ angular.module('tripPlannerApp')
 
     ngGPlacesAPI.placeDetails({reference:"CnRnAAAAnRm_imIW_SFd74bsj6iRwvRxBamZqtUaSyRjlb-i1vvkapOSVXyA5Dj452GSpBpno_MHbxyGsuFx9zqZvr_aa2a7uG0IZE8tC-N2OccvUC_i5N3QRQ11WmSRayo441riHebwQGqlbaf3RY-5KVsfGBIQXGtmUICHsD9LH2rd_y-J2hoUvW0lUEIHHtRnD15QyeUqi6tkHIg"})
         .then(function (data) {
-            console.log("Details: ", data);
             $scope.details = data;
         });
     ngGPlacesAPI.nearbySearch({latitude:-33.8665433, longitude:151.1956316})
         .then(function(data){
-            console.log("Data: ", data);
             $scope.places = data;
         }); 
+
+    $scope.toggleView = function() {
+        console.log("clicked");
+    }
 });
