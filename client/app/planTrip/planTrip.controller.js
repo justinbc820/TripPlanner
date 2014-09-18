b6fb8f485f6e576f6941fec12bb364ea5aa097e7
Justin <justinbchristensen@gmail.com>
2014-09-17 23:37:01 -0400

    FINALLY got google places working with a text search

--- client/app/planTrip/planTrip.controller.js
+++ client/app/planTrip/planTrip.controller.js
@@ -14,13 +14,4 @@ angular.module('tripPlannerApp')
     $scope.places;
 
     $scope.toggleView = ToggleView.toggleView;
-
-    ngGPlacesAPI.placeDetails({reference:"CnRnAAAAnRm_imIW_SFd74bsj6iRwvRxBamZqtUaSyRjlb-i1vvkapOSVXyA5Dj452GSpBpno_MHbxyGsuFx9zqZvr_aa2a7uG0IZE8tC-N2OccvUC_i5N3QRQ11WmSRayo441riHebwQGqlbaf3RY-5KVsfGBIQXGtmUICHsD9LH2rd_y-J2hoUvW0lUEIHHtRnD15QyeUqi6tkHIg"})
-        .then(function (data) {
-            $scope.details = data;
-        });
-    ngGPlacesAPI.nearbySearch({latitude:-33.8665433, longitude:151.1956316})
-        .then(function(data){
-            $scope.places = data;
-        });
 });
