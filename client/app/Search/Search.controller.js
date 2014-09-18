93ba9324ce8b14ef1ebbd4e8a6424517a0dbbb9d
Justin <justinbchristensen@gmail.com>
2014-09-18 16:29:03 -0400

    Fully integrated GooglePlaces into search

--- client/app/Search/Search.controller.js
+++ client/app/Search/Search.controller.js
@@ -7,26 +7,28 @@ angular.module('tripPlannerApp')
 
   	$scope.details;
   	$scope.places;
+    $scope.returnedPlaces = [];
 
-    $scope.gMapsSearch = function(searchQuery) {
+    $scope.gMapsSearch = function(autocomplete) {
       // ngGPlacesAPI.setDefaults({types:['lodging']});
       // ngGPlacesAPI.nearbySearch({latitude:-33.8665433, longitude:151.1956316}).then(
       //     function(data){
       //       $scope.places = data;
       //     });
-      ngGPlacesAPI.textSearch({'query':searchQuery}).then(
+      ngGPlacesAPI.textSearch({'query':autocomplete}).then(
           function(data){
             $scope.places = data;
           });
-    };
 
-    // ngGPlacesAPI.placeDetails({reference:"CnRnAAAAnRm_imIW_SFd74bsj6iRwvRxBamZqtUaSyRjlb-i1vvkapOSVXyA5Dj452GSpBpno_MHbxyGsuFx9zqZvr_aa2a7uG0IZE8tC-N2OccvUC_i5N3QRQ11WmSRayo441riHebwQGqlbaf3RY-5KVsfGBIQXGtmUICHsD9LH2rd_y-J2hoUvW0lUEIHHtRnD15QyeUqi6tkHIg"})
-    //     .then(function (data) {
-    //         $scope.details = data;
-    //     });
-    // ngGPlacesAPI.nearbySearch({latitude:-33.8665433, longitude:151.1956316}).then(
-    //     function(data){
-    //       $scope.places = data;
-    //     });
-        
+    }; 
+
+    $scope.placeDetails = function(place) {
+      var placeObj = {
+        placeId: place.place_id
+      };
+      ngGPlacesAPI.placeDetails(placeObj).then(
+          function(data){
+            $scope.details = data;
+          });
+    } 
   });
